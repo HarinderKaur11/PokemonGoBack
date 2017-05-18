92015cf5c0c1a9715a167af5736833ec8b967405
Navsingh <navsingh191@gmail.com>
2017-05-18 14:52:43 -0400

    Main Interface

---
+++ src/application/Controller.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+
+public class Controller
+{
+     @FXML
+    private Button b1;
+
+
+}
