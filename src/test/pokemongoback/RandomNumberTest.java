6bb43b011891e342f789df3f9e153fff4b6c1d6f
novadhaliwal <boparainova@gmail.com>
2017-05-22 17:15:56 -0400

    tests

---
+++ src/test/pokemongoback/RandomNumberTest.java
@@ -0,0 +1,17 @@
+package test.pokemongoback;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RandomNumberTest {
+
+	@Test
+	public void test() {
+		
+		
+		
+		
+	}
+
+}
