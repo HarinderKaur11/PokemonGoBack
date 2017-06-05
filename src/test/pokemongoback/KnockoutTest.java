02d4f7f3037117c5f7ceb07393be56e0f963b881
novadhaliwal <boparainova@gmail.com>
2017-06-05 17:24:12 -0400

    new tests

---
+++ src/test/pokemongoback/KnockoutTest.java
@@ -0,0 +1,22 @@
+package test.pokemongoback;
+
+
+import org.junit.Test;
+
+public class KnockoutTest {
+
+	@Test
+	public void test() {
+		
+		
+	/*	public void addDamage(int newDamage){
+			this.damage += newDamage;
+			if(this.damage==this.hitpoints || this.damage>=this.hitpoints){
+				this.state = "knockedOut";
+			}
+		
+		*/
+		
+	}
+
+}
