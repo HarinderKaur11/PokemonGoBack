e61a46f38ddd8f24fe445726e41da6848af7eb53
amittbhardwj <amitt.bhardwj@gmail.com>
2017-06-04 21:10:42 -0400

    Deck parsed Abilities

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -19,15 +19,25 @@ public class Deck extends CardsGroup{
 		for(String[] cards : cardsList){
 			//int n = Integer.parseInt(cards[0]);
 			//Debug.message(n+" "+cards[1]);
-			//for(int i=1;i<=n;i++){
-				switch(cards[2]){
+			//for(int i=1;i<=n;i++){\
+			
+			if(cards[1]=="pokemon"){
+				Debug.message("Hello checkpoint");
+			}
+			
+				switch(cards[1]){
 					
 					case "pokemon":
-						if (cards[3] == "basic"){
-						this.getGroupCards().add(new Pokemon(x, cards[0], stage, Integer.parseInt(cards[6]), newAbility));
+						if(cards[3].equals("basic")){
+							//Debug.message("checkpoint3");
+							this.getGroupCards().add(new Pokemon(x, cards[0], stage, Integer.parseInt(cards[6]), newAbility));
 						}
-						else {
+						else if(cards[3].equals("stage-one")){
+							//Debug.message(cards[3] + cards[6]);
 							this.getGroupCards().add(new Pokemon(x, cards[0], new stageOnePokemon(cards[4]), Integer.parseInt(cards[7]), newAbility));
+						}
+						else{
+							Debug.message("Not Running " + cards[3]);
 						}
 						break;
 					case "trainer":
