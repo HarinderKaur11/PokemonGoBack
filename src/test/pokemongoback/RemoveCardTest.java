8ee93119864ac58445657db7d828d815c26eadd2
Gurpreet Raju <gurpreetraj92@gmail.com>
2017-06-09 17:49:20 -0400

    nothing special

--- src/test/pokemongoback/RemoveCardTest.java
+++ src/test/pokemongoback/RemoveCardTest.java
@@ -1,7 +1,11 @@
 package test.pokemongoback;
 import controller.GameController;
 import org.omg.Messaging.SyncScopeHelper;
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 import model.UserPlayer;
 import model.cardItem;
@@ -10,8 +14,13 @@ public class RemoveCardTest {
 
 	HBox panel;
 	
-	addCardsToPanel(card, panel);
-
+	@Test
+	public void TestRemoveCard(){
+		Node expected = null;
+		GameController.getInstance().addCardsToPanel(null, panel);
+		Node actual = panel.getChildren().get(0);
+		assertEquals(expected,actual);
+	}
 }
 
  
