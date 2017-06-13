81d8a442ef596a1c454c01a8702edafcf765b296
amittbhardwj <amitt.bhardwj@gmail.com>
2017-06-13 15:20:42 -0400

    Swap and Draw ability implemented

--- src/model/swapAbility.java
+++ src/model/swapAbility.java
@@ -1,9 +1,17 @@
 package model;
 
+import controller.GameController;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
 public class swapAbility extends ability{
 	private String name;
 	private String activepokemon;
 	private String benchpokemon;
+	private AIplayer ai;
+	private UserPlayer user;
+	@FXML HBox userActivePokemon;
+	@FXML HBox aiActivePokemon;
 	
 	public swapAbility(String name,String activepokemon, String benchpokemon){
 		this.name = name;
@@ -19,21 +27,40 @@ public class swapAbility extends ability{
 	
 	public void useAbility() {
 		
-		Player player = Turn.getInstance().getCurrentPlayer();
+		Player player = (Player) getPlayer(benchpokemon);
+		//CardsGroup benchcards = player.getBench();
+		cardItem active = (Pokemon) target.getTargetObject(activepokemon).getTarget();
+		Pokemon bench = (Pokemon) target.getTargetObject(benchpokemon).getTarget(); 
 		CardsGroup benchcards = player.getBench();
-		cardItem active = player.getActivePokemon();
 		//PokemonCard activeCard = new PokemonCard(active, userActivePokemon);
-		for(cardItem card: benchcards.getCard()){
-			//Do code here
-			//if(){
-				player.setActivePokemon((Pokemon)card);
-			//}
-		}
+//		for(cardItem card: benchcards.getCard()){
+//			//Do code here
+//			//if(){
+//				player.setActivePokemon((Pokemon)card);
+//			//}
+//		}
 		benchcards.addCard(active);
-		
+		benchcards.removeCard(bench);
+		player.setActivePokemon(bench);
+		GameController.getInstance().addCardToPanel(active, GameController.getInstance().getBench(player));
+		GameController.getInstance().removeCard(String.valueOf(bench.getID()), GameController.getInstance().getBench(player));
+		if (player.equals(user)){
+			
+			GameController.getInstance().addCardToPanel(bench, userActivePokemon);
+		}else{
+			GameController.getInstance().addCardToPanel(bench, aiActivePokemon);
+		}
 			
 		}
 		
+	public Player getPlayer(String playertarget){
+		if(playertarget.equals("yourbench")){
+			return Turn.getInstance().getCurrentPlayer();
+		}
+		else{
+			return Turn.getInstance().getOpponent();
+		}
+	}
 	}
 	
 
