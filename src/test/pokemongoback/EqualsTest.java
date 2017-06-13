d8b904b17ac05eaf0cf07767b97e52b957f088c5
Harinder Kaur <snazyahuja11@gmail.com>
2017-06-13 11:38:39 -0400

    Test for equals(object) method of DeckAbility class

---
+++ src/test/pokemongoback/EqualsTest.java
@@ -0,0 +1,34 @@
+package test.pokemongoback;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.CardsGroup;
+import model.DeckAbility;
+import model.ability;
+
+public class EqualsTest {
+	
+	Boolean actual;
+	Boolean expected;
+	
+	@Test
+	public void test() {
+		DeckAbility o1=new DeckAbility("Flash", "raichu", "opponentActive", true, 10);
+		CardsGroup cg=new CardsGroup();
+		expected=false;
+		actual=o1.equals(cg);
+		
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void test1() {
+		DeckAbility o2=new DeckAbility("Flash", "raichu", "opponentActive", true, 10);
+		actual=o2.equals(o2);
+		expected=true;
+		assertEquals(expected,actual);
+		}
+
+}
