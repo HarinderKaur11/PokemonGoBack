aa68e478aa31973fefd56de7e95b6cc8901faf28
Gurpreet Raju <gurpreetraj92@gmail.com>
2017-06-16 01:07:45 -0400

    updated deck ability

--- src/model/DeckAbility.java
+++ src/model/DeckAbility.java
@@ -4,28 +4,25 @@ import controller.GameController;
 
 public class DeckAbility extends ability {
 
-	private String targetDestination, count;
+	private String targetDestination;
 	private int amount;
-	private boolean choice;
+	private String choice;
 	
-	public DeckAbility(String newName, String newTarget, String newTargetDestination, boolean newChoice, int newAmount, String count){
+	public DeckAbility(String newName, String newTarget, String newTargetDestination, int newAmount, String newChoice){
 		this.name = newName;
 		this.abilitytarget = newTarget;
 		this.targetDestination = newTargetDestination;
 		this.amount = newAmount;
-		this.choice = newChoice;
-		this.count = count; //count = yourhand, opponenthand
+		this.choice = newChoice; //count = yourhand, opponenthand
 	}
 	
 	public void useAbility() {
-		if(choice){
-			for(int i=0;i<amount;i++){
-				cardItem nCard = GameController.getInstance().getPanelPokemonDialog((Player) target.getTargetObject(this.abilitytarget).getTarget(), "hand");
-				((CardsGroup) this.getTargetLocation(this.targetDestination,this.abilitytarget)).addCard(nCard);
-			}
+		if(choice!=null){
+				this.amount = ((CardsGroup) this.getTargetLocation("hand", this.choice)).getGroupCards().size();
 		}
-		else{
-			
+		for(int i=0;i<amount;i++){
+			cardItem nCard = ((CardsGroup) this.getTargetLocation("hand", this.abilitytarget)).getGroupCards().remove(0);
+			((CardsGroup) this.getTargetLocation(this.targetDestination,this.abilitytarget)).addCard(nCard);
 		}
 	}
 	
