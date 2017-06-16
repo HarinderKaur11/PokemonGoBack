4f2df4c80dfcae755a8c5459a3ac87715ca0c851
Harinder Kaur <snazyahuja11@gmail.com>
2017-06-16 08:00:46 -0400

    Adding stubs

---
+++ src/test/stubs/ability.java
@@ -0,0 +1,36 @@
+package test.stubs;
+
+import model.Player;
+import model.target;
+
+public abstract class ability {
+	
+	protected String name;
+	protected String abilitytarget;
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public target getTargetObject(){
+		return target.getTargetObject(this.abilitytarget);
+	}
+	
+	public Object getTargetLocation(String source, String newtarget){
+		Player player = (Player) target.getTargetObject(newtarget).getTarget();
+		switch(source){
+			case "deck":
+				return player.getDeck();
+			case "discard":
+				return player.getDiscardPile();
+			case "hand":
+				return player.getInhand();
+			default:
+				return null;
+		}
+	}
+	
+	public abstract boolean equals(Object o);
+	public abstract void useAbility();
+
+}
--- src/test/stubs/condAbility.java
+++ src/test/stubs/condAbility.java
@@ -2,12 +2,29 @@ package test.stubs;
 
 import java.util.ArrayList;
 
-import model.Energy;
-import model.Turn;
+import model.AbilityParser;
 import model.UserPlayer;
+import model.ability;
 
-public class condAbility extends model.ability{
+public class condAbility extends ability{
 
+	/*cond:flip:dam:target:opponent-active:30
+		cond:flip:applystat:status:paralyzed:opponent-active
+		cond:flip:dam:target:opponent-active:10
+		cond:healed:target:your-active:dam:target:opponent-active:80
+		cond:flip:deenergize:target:opponent-active:1
+		cond:flip:dam:target:opponent-active:30
+		cond:flip:dam:target:opponent-active:20
+		cond:flip:dam:target:opponent-active:40:else:applystat:status:paralyzed:opponent-active
+		cond:flip:deenergize:target:opponent-active:1
+		cond:ability:deck:destination:discard:target:choice:you:1:(search:target:you:source:deck:filter:top:8:1,shuffle:target:you)
+		cond:count(target:your-active:energy:psychic)>0:dam:target:opponent-active:20
+		cond:flip:dam:target:opponent-active:30:else:dam:target:your-active:30
+		cond:flip:dam:target:opponent-active:10
+		cond:flip:(applystat:status:asleep:opponent-active,applystat:status:poisoned:opponent-active)
+		cond:choice:shuffle:target:opponent
+		cond:flip:heal:target:your-active:10
+	 */
 	private UserPlayer user;
 	private String condition, ability;
 	private ArrayList<Energy> EnergyInfo;
@@ -21,19 +38,10 @@ public class condAbility extends model.ability{
 	}
 	
 	public void useAbility() {
-		DeckFileReader db;
-		if(Turn.getInstance().getCurrentPlayer() == user)
-		{
-			db = new DeckFileReader(2);
-		}
-		else
-		{
-			db = new DeckFileReader(1);
-		}
-		
+		AbilityParser ap = new AbilityParser();
 		if(condition.equals("flip"))
 		{
-			db.getAbility(name, ability.split(" "), EnergyInfo);
+			ap.getAbility(name, ability.split(" "), EnergyInfo);
 		}
 		else if(condition.equals("choice"))
 		{
